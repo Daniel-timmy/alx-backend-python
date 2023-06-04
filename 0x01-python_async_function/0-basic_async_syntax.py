7f69295766760424092a91e7c22f0d42c8b3fe37
Daniel-timmy <ajayitimmy45@gmail.com>
2023-06-04 18:12:10 +0100

    required change to type annotation

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -2,10 +2,9 @@
 """asynchronous coroutine that takes in an integer argument"""
 import asyncio
 import random
-from typing import Union
 
 
-async def wait_random(max_delay=10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """ waits for a random delay between 0 and max_delay
     seconds and eventually returns it"""
     i = random.random() * max_delay
